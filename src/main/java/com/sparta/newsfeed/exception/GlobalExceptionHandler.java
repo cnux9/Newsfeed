44b0b3ea633aa75529e571675517bbbf24a86e3a
mayfly <dngurrb1004@naver.com>
2024-12-23 14:49:31 +0900

    add global exception handler

---
+++ src/main/java/com/sparta/newsfeed/exception/GlobalExceptionHandler.java
@@ -0,0 +1,14 @@
+package com.sparta.newsfeed.exception;
+
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.http.ResponseEntity;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(CustomException.class)
+    public ResponseEntity<ErrorResponse> handleCustomException(CustomException ex) {
+        return ResponseEntity.status(ex.getHttpStatus())
+                .body(new ErrorResponse(ex.getErrorCode(), ex.getMessage()));
+    }
+}
